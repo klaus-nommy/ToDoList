6f05d35734916cc07a7c042f7d8c551f665079f2
klaus-nommy <31960430+klaus-nommy@users.noreply.github.com>
2017-09-17 15:31:44 +0900

    second day

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Logic from './Logic.js';
+//import Logic from './Logic.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>ToDo List</h2>
         </div>
 <logic>
-
+<form>
+  <label>
+    ToDo:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
 </logic>
       </div>
     );
